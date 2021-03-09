d5297dd7d0c010145e1cc744df5d0dc14a7e33ba
brunoquei2105 <bruno.queiroz@liferay.com>
2021-03-09 10:38:29 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML ="Hello World";
+
+function change(id){
+    return id.innerHTML = Date()
+};
+
+function mouseover(){
+    return document.getElementById("demo3").innerHTML= "Você está contratado"
+    
+}
